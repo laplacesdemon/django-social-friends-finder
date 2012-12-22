a767c2802ae61aacf540fa83b2ed05c7c75ceb16
Suleyman Melikoglu <suleymanmelikoglu@gmail.com>
2012-12-23 01:12:29 +0200

    setup.py version is increased

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import find_packages
 
 setup(
     name='django-social-friends-finder',
-    version='0.1',
+    version='0.2',
     author=u'Suleyman Melikoglu',
     author_email='suleyman@melikoglu.info',
     packages=find_packages(),
     include_package_data=True,
     url='https://github.com/laplacesdemon/django-social-friends-finder.git',
     license='BSD licence, see LICENCE.txt',
-    description='An extension app for django-social-auth that fetches your friends from different social-networks.',
+    description='An extension app for django-social-auth or django-allauth that fetches your friends from different social-networks.',
     long_description=open('README.md').read(),
     zip_safe=False,
 )
