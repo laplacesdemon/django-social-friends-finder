c45ab4be752ded88c4bd5d6b7c05c442ceb2a476
Suleyman Melikoglu <suleymanmelikoglu@gmail.com>
2012-12-23 01:05:44 +0200

    urls import support for django 1.3

--- social_friends_finder/urls.py
+++ social_friends_finder/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import *
 from social_friends_finder.views import FriendListView
 from django.contrib.auth.decorators import login_required
 
